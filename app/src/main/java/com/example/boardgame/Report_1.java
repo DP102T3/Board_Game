e57309b4d1607a28bff81f7bb2dd414ebb12f7d5
oolongtea <leo810111@gmail.com>
2019-09-24 15:29:51 +0800

    navigation建立

---
+++ app/src/main/java/com/example/boardgame/Report_1.java
@@ -0,0 +1,31 @@
+package com.example.boardgame;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Report_1 extends Fragment {
+
+
+    public Report_1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_report_1, container, false);
+    }
+
+}
